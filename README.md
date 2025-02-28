bf5b257d4836a04b8b836f43617ea811bddb5178
Soulter <905617992@qq.com>
2025-02-28 13:58:56 +0800

    update

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
-# helloworld
+# astrbot-github-cards
 
-AstrBot 插件模板
+根据群聊中 GitHub 相关链接自动发送 GitHub OpenGraph 图片
 
-A template plugin for AstrBot plugin feature
+## 功能介绍
 
-# 支持
+这是一个 AstrBot 插件，可以自动识别群聊中的 GitHub 链接并发送相应的 OpenGraph 预览图片。支持以下类型的链接：
+- 仓库链接：`https://github.com/username/repository`
+- Issues 链接：`https://github.com/username/repository/issues/number`
+- Pull Requests 链接：`https://github.com/username/repository/pull/number`
 
-[帮助文档](https://astrbot.app)
+## 使用方法
+
+无需任何命令，只需在聊天中发送 GitHub 链接即可自动触发预览图片的生成和发送。
+
+## 示例
+
+在聊天中发送：
+```
+https://github.com/Soulter/AstrBot
+```
+
+插件会自动识别链接并发送包含仓库信息的预览图片。
+
+## 计划功能
+
+未来计划添加以下功能：
+- GitHub Star 历史趋势图查看命令 `/ghstar`
+
+## 支持与贡献
+
+如有问题或建议，请访问插件仓库提交 issue。
+
+[AstrBot 帮助文档](https://astrbot.app)
--- main.py
+++ main.py
@@ -1,18 +1,52 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+import re
+# import os
+import uuid
+# from PIL import Image
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+# from astrbot.core.utils.io import download_file
 
-@register("helloworld", "Your Name", "一个简单的 Hello World 插件", "1.0.0", "repo url")
+GITHUB_URL_PATTERN = r"https://github\.com/[\w\-]+/[\w\-]+(?:/(pull|issues)/\d+)?"
+GITHUB_REPO_OPENGRAPH = "https://opengraph.githubassets.com/{hash}/{appendix}"
+
+STAR_HISTORY_URL = "https://api.star-history.com/svg?repos={identifier}&type=Date"
+
+@register("astrbot_plugin_github_cards", "Soulter", "根据群聊中 GitHub 相关链接自动发送 GitHub OpenGraph 图片", "1.0.0", "https://github.com/Soulter/astrbot_plugin_github_cards")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
+
+
+    @filter.regex(GITHUB_URL_PATTERN)
+    async def github_repo(self, event: AstrMessageEvent):
+        '''解析 Github 仓库信息'''
+        msg = event.message_str
+        match = re.search(GITHUB_URL_PATTERN, msg)
+        repo_url = match.group(0)
+        repo_url = repo_url.replace("https://github.com/", "")
+        hash_value = uuid.uuid4().hex
+        opengraph_url = GITHUB_REPO_OPENGRAPH.format(hash=hash_value, appendix=repo_url)
+        logger.info(f"生成的 OpenGraph URL: {opengraph_url}")
+
+        try:
+            yield event.image_result(opengraph_url)
+        except Exception as e:
+            logger.error(f"下载图片失败: {e}")
+            yield event.plain_result("下载 GitHub 图片失败: " + str(e))
+            return
     
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        '''这是一个 hello world 指令''' # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+    # TODO: svg2png
+    # @filter.command("ghstar")
+    # async def ghstar(self, event: AstrMessageEvent, identifier: str):
+    #     '''查看 GitHub 仓库的 Star 趋势图。如: /ghstar Soulter/AstrBot'''
+    #     url = STAR_HISTORY_URL.format(identifier=identifier)
+    #     # download svg
+    #     fpath = "data/temp/{identifier}.svg".format(identifier=identifier.replace("/",
+    #         "_"))
+    #     await download_file(url, fpath)
+    #     # convert to png
+    #     png_fpath = fpath.replace(".svg", ".png")
+    #     cairosvg.svg2png(url=fpath, write_to=png_fpath)
+    #     # send image
+    #     yield event.image_result(png_fpath)
